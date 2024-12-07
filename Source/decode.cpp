d7012f97b7be74bef82e32ce838b72884a63a4eb
EricShinozaki <emfshinozaki@gmail.com>
2024-12-06 23:21:05 -0800

    Send encoded data in base64 to prevent data loss. Start decode method

--- Source/decode.cpp
+++ Source/decode.cpp
@@ -1,7 +1,15 @@
 #include "main.h"
 
-std::string decode(std::string decodedText)
+std::string decode(std::string encryptedMessage)
 {
+	int firstColon = encryptedMessage.find("|");
+	int secondColon = encryptedMessage.find("|", firstColon + 1);
 
-	return decodedText;
+	std::string encodedMessage = encryptedMessage.substr(0, firstColon);
+	std::string base64EncodedKey = encryptedMessage.substr(firstColon + 1, secondColon);
+
+	std::cout << "Encoded Message: " << encodedMessage << std::endl;
+	std::cout << "Base64 Encoded Key: " << base64EncodedKey << std::endl;
+
+	return encryptedMessage;
 }
--- Source/encode.cpp
+++ Source/encode.cpp
@@ -8,15 +8,15 @@ std::string encode()
     const unsigned char* messageAsUnsignedChar = reinterpret_cast<const unsigned char*>(messageToEncode.c_str());
 
     // Necessary AES encryption variables
-    unsigned char key[32], IV[16];
+    unsigned char key[32]; //, IV[16];
     EVP_CIPHER_CTX* encryption_ctx = EVP_CIPHER_CTX_new();
 
     // Assign random values to our key and IV
     RAND_bytes(key, 32);
-    RAND_bytes(IV, 16);
+    // RAND_bytes(IV, 16); - might be fine to do just ECB 
 
     // Initialize the AES-256-CBC cipher
-    EVP_EncryptInit_ex(encryption_ctx, EVP_aes_256_cbc(), NULL, key, IV);
+    EVP_EncryptInit_ex(encryption_ctx, EVP_aes_256_ecb(), NULL, key, NULL);
 
     int input_len = messageToEncode.length();
     int c_len = input_len + AES_BLOCK_SIZE;
@@ -37,17 +37,25 @@ std::string encode()
         EVP_CIPHER_CTX_free(encryption_ctx);
         return "";
     }
+
     encrypted_len += out_len;
 
-    // Convert to std::string
-    std::string encodedText(reinterpret_cast<char*>(ciphertext), encrypted_len);
+    // base64 encode the ciphertext
+    std::vector<unsigned char> encodedCiphertext((4 * (encrypted_len + 2)));
+    int encoded_len = EVP_EncodeBlock(encodedCiphertext.data(), ciphertext, encrypted_len);
+
+    // base64 encode the key
+    std::vector<unsigned char> encodedKey((4 * (32 + 2)));
+    int encoded_key_len = EVP_EncodeBlock(encodedKey.data(), key, 32);
 
     // Clean up resources
     delete[] ciphertext;
     EVP_CIPHER_CTX_free(encryption_ctx);
 
-	
-	return encodedText;
+    std::string encodedCiphertextString(reinterpret_cast<char*>(encodedCiphertext.data()), encoded_len);
+    std::string keyAsString(reinterpret_cast<char*>(encodedKey.data()), encoded_key_len);
+
+    return  encodedCiphertextString + "|" + keyAsString;
 }
 
 
